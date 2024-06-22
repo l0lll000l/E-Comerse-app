3d72e202c2ca6d9ca49e7c4ff361bab8c82407d3
Athul <athulmp86@gmail.com>
2024-06-22 20:14:23 +0530

    colors added

---
+++ lib/Utils/constants/colors.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class TColors {
+  TColors._();
+  // App  Basic Colors
+  static const Color primary = Colors.lightBlue;
+  static const Color secondary = Colors.yellow;
+  static const Color accent = Colors.grey;
+// Gradient color
+  static const Gradient lineargradient = LinearGradient(
+      end: Alignment(0.707, -0.707),
+      begin: Alignment(0.0, 0.0),
+      colors: [
+        Colors.orange,
+        Colors.orangeAccent,
+        Colors.deepOrange,
+        Colors.deepOrangeAccent
+      ]);
+  // Text Color
+  static const Color textPrimary = Color.fromARGB(136, 0, 0, 0);
+  static const Color textSeconsary = Color.fromARGB(255, 45, 43, 43);
+  static const Color textwhite = Colors.white;
+  // Background color
+  static const Color light = Colors.white;
+  static const Color dark = Colors.black;
+  static const Color primarybackground = Colors.white;
+  //background container
+  static const Color lightContainer = Colors.white;
+  static const Color darkContainer = Colors.black;
+  // Button color
+  static const Color buttonPrimary = Color.fromARGB(255, 3, 34, 173);
+  static const Color buttonSecondary = Colors.grey;
+  static const Color buttonDisabled = Colors.white;
+  // Border color
+  static const Color borderPrimary = Colors.grey;
+  static const Color borderSecondary = Colors.grey;
+
+  //Error and validationcolors
+  static const Color error = Colors.red;
+  static const Color success = Colors.green;
+  static const Color warning = Colors.orange;
+  static const Color info = Colors.blue;
+  //neutral shadows
+  static const Color black = Colors.black;
+  static const Color darkerGrey = Color.fromARGB(255, 41, 39, 39);
+  static const Color darkGrey = Color.fromARGB(255, 56, 52, 52);
+  static const Color grey = Colors.grey;
+}
