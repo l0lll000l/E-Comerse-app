bfb1721a0c4f72761bd870c27d884d7c2c24df05
Athul <athulmp86@gmail.com>
2024-06-22 16:59:10 +0530

    home page added

--- lib/home.dart
+++ lib/home.dart
@@ -1,38 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/auth/auth.dart';
-import 'package:flutter_application_1/helper/databaseService.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  AuthServices authServices = AuthServices();
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Text(''),
-          ElevatedButton(
-              onPressed: () {
-                AuthServices().register();
-              },
-              child: Text('register')),
-          ElevatedButton(onPressed: () {}, child: Text('login')),
-          ElevatedButton(
-              onPressed: () {
-                DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
-                    .createGroup(id: FirebaseAuth.instance.currentUser!.uid);
-              },
-              child: Text('create group')),
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
