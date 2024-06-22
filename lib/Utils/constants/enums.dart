fe7b49e5967cef86d82de39199b9153399055731
Athul <athulmp86@gmail.com>
2024-06-22 22:07:57 +0530

    formatter setup

---
+++ lib/Utils/constants/enums.dart
@@ -0,0 +1,3 @@
+enum TextSizes{small,medium,large}
+enum OrderStatus{processing,shipped,delivered}
+enum PaymentMethods {paypal,googlePay,applePay,visa,mastercard,creditcard,paystack,razorPay,paytm}
---
+++ lib/Utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+/// This class contains all the App Images in String formats.
+class TImages {
+// App Logos
+  static const String darkAppLogo =
+      "assets/logos/t-store-splash-logo-black.png";
+  static const String lightAppLogo =
+      "assets/logos/t-store-splash-logo-white.png";
+// Social Logos
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+}
